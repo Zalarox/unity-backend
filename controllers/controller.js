e772aeb3c09a39dface6cce8d145cfdff7fd1ad4
Bansal <sbansal@na.global.ad>
2017-08-02 18:22:42 +0530

    authentication service

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,9 @@ var express = require('express'),
  r = require('rethinkdb'),
  router = express.Router(),
  bodyParser = require('body-parser'),
+ jwt = require('jsonwebtoken'),
  connection = null,
+ config = require('./../config'),
  User = require('./../models/User');
 
 r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
@@ -13,6 +15,52 @@ r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
 
 router.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Authentication service 
+router.post('/gettoken', function(req, res) {
+	if(req.body.name === "Siddhant" && req.body.password === "password123") {
+
+		var payload = { name: req.body.name, password: req.body.password };
+
+		var token = jwt.sign(payload, config.secret, { 
+				expiresIn: '24h'
+			});
+
+		res.json({
+			success: true,
+			token: token
+		});
+	}
+
+	res.json({
+		success: false,
+		token: null
+	});
+});
+
+// Middleware
+router.use(function(req, res, next) {
+
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	if (token) {
+		jwt.verify(token, config.secret, function(err, decoded) {      
+		  if (err) {
+		    return res.json({ success: false, message: 'Failed to authenticate token.' });    
+		  } else {
+		    req.decoded = decoded;    
+		    next();
+		  }
+		});
+
+	} 
+	else {
+		return res.status(403).send({ 
+		    success: false, 
+		    message: 'No token provided.' 
+		});
+	}
+});
 
 router.post('/new', function (req, res) {
 	var user = new User(req.body);
