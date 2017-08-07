9aeef38ec305ed6873ea6b519040406eeaf09fe9
Bansal <sbansal@na.global.ad>
2017-08-07 18:17:00 +0530

    added new models

--- models/ARData.js
+++ models/ARData.js
@@ -2,7 +2,7 @@ var thinky = require('thinky')({ db: "DataStore" });
 var r = thinky.r;
 var type = thinky.type;
 
-var User = thinky.createModel("data", {
+var User = thinky.createModel("ARData", {
     name: type.string(),
     password: type.string(),
     created: type.date().default(r.now())
---
+++ models/ShapeTrackerData.js
@@ -0,0 +1,13 @@
+var thinky = require('thinky')({ db: "DataStore" });
+var r = thinky.r;
+var type = thinky.type;
+
+var ShapeTrackerData = thinky.createModel("ShapeTrackerData", {
+    name: type.string(),
+    score: type.number(),
+    gametype: type.string(),
+    totalattempts: type.number(),
+    created: type.date().default(r.now())
+});
+
+module.exports = ShapeTrackerData;
