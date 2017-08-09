a8af8c55641dacf15d85b5d01a54ac15c5498343
Bansal <sbansal@na.global.ad>
2017-08-09 17:46:56 +0530

    Added builder game model

---
+++ models/BuilderData.js
@@ -0,0 +1,12 @@
+var thinky = require('thinky')({ db: "DataStore" });
+var r = thinky.r;
+var type = thinky.type;
+
+var User = thinky.createModel("BuilderData", {
+    name: type.string(),
+    score: type.number(),
+    levels: type.number(),
+    created: type.date().default(r.now())
+});
+
+module.exports = User;
